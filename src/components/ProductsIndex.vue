ce93cbfe045800c8619875a0ac843fc4a979bcb4
realcdugz <connorduggan30@gmail.com>
2024-12-26 13:33:40 -0600

    Added Show, Edit/Update, and Destroy actions

---
+++ src/components/ProductsIndex.vue
@@ -0,0 +1,22 @@
+<script>
+export default {
+  props: {
+    photos: Array,
+  },
+};
+</script>
+
+<template>
+  <div>
+    <h1>All products</h1>
+    <div v-for="product in products" v-bind:key="product.id">
+      <h2>{{ product.name }}</h2>
+      <img v-bind:src="product.url" />
+      <p>Width: {{ product.width }}</p>
+      <p>Height: {{ product.height }}</p>
+      + <button v-on:click="$emit('showProduct', product)">More info</button>
+    </div>
+  </div>
+</template>
+
+<style></style>
--- src/components/ProductsPage.vue
+++ src/components/ProductsPage.vue
@@ -1,8 +1,98 @@
-<script></script>
+<script>
+import axios from "axios";
+import ProductsIndex from "./ProductsIndex.vue";
+import ProductsNew from "./ProductsNew.vue";
+import ProductsShow from "./ProductsShow.vue";
+import Modal from "./Modal.vue";
+
+export default {
+  components: {
+    ProductsIndex,
+    ProductsNew,
+    ProductsShow,
+    Modal,
+  },
+  data: function () {
+    return {
+      products: [],
+      currentProduct: {},
+      isProductsShowVisible: false,
+    };
+  },
+  created: function () {
+    this.handleIndexProducts();
+  },
+  methods: {
+    handleIndexProducts: function () {
+      axios.get("/products.json").then((response) => {
+        console.log("products index", response);
+        this.products = response.data;
+      });
+    },
+    handleCreateProduct: function (params) {
+      axios
+        .post("/products.json", params)
+        .then((response) => {
+          console.log("products create", response);
+          this.products.push(response.data);
+        })
+        .catch((error) => {
+          console.log("products create error", error.response);
+        });
+    },
+    handleShowProduct: function (product) {
+      console.log("handleShowProduct", product);
+      this.currentProduct = product;
+      this.isProductsShowVisible = true;
+    },
+    handleUpdateProduct: function (id, params) {
+      console.log("handleUpdateProduct", id, params);
+      axios
+        .patch(`/products/${id}.json`, params)
+        .then((response) => {
+          console.log("products update", response);
+          this.products = this.products.map((product) => {
+            if (product.id === response.data.id) {
+              return response.data;
+            } else {
+              return product;
+            }
+          });
+          this.handleClose();
+        })
+        .catch((error) => {
+          console.log("products update error", error.response);
+        });
+    },
+    handleDestroyProduct: function (product) {
+      axios.delete(`/products/${product.id}.json`).then((response) => {
+        console.log("products destroy", response);
+        var index = this.products.indexOf(product);
+        this.products.splice(index, 1);
+        this.handleClose();
+      });
+    },
+    handleClose: function () {
+      this.isProductsShowVisible = false;
+    },
+  },
+};
+</script>
 
 <template>
   <main>
-    <h1>Welcome to Vue!</h1>
+    <ProductsNew v-on:createProduct="handleCreateProduct" />
+    <ProductsIndex
+      v-bind:products="products"
+      v-on:showProduct="handleShowProduct"
+    />
+    <Modal v-bind:show="isProductsShowVisible" v-on:close="handleClose">
+      <ProductsShow
+        v-bind:product="currentProduct"
+        v-on:updateProduct="handleUpdateProduct"
+        v-on:destroyProduct="handleDestroyProduct"
+      />
+    </Modal>
   </main>
 </template>
 
