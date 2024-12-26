43ee5851602c10d94e39431b983105b08b873756
Elijah Gutman <gutmane7@gmail.com>
2024-12-26 14:44:55 -0500

    finished index action

---
+++ src/components/ProductsIndex.vue
@@ -0,0 +1,26 @@
+<script>
+export default {
+  props: {
+    products: {
+      type: Array,
+    },
+  },
+};
+</script>
+
+<template>
+  <div>
+    <h1>All products</h1>
+    <div v-for="product in products" v-bind:key="product.id">
+      <h2>{{ product.name }}</h2>
+      <img v-bind:src="product.image_url" />
+      <p>Description: {{ product.description }}</p>
+      <p>Category: {{ product.category }}</p>
+      <p>Price: {{ product.price }}</p>
+      <p>Tax Rate: {{ product.tax }}</p>
+      <p>Total: {{ product.total }}</p>
+    </div>
+  </div>
+</template>
+
+<style></style>
