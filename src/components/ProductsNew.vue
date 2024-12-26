43d9ba04ae88a246a9f3eea9027a0761cdac2391
tommycrowley10 <tommycrowley10@gmail.com>
2024-12-26 13:55:51 -0600

    Newly updated create action'

--- src/components/ProductsNew.vue
+++ src/components/ProductsNew.vue
@@ -19,20 +19,32 @@ export default {
     <h1>New Product</h1>
     <form v-on:submit.prevent="handleSubmit">
       <div>
-        Name:
+        name:
         <input type="text" v-model="newProductParams.name" />
       </div>
       <div>
-        Url:
-        <input type="text" v-model="newProductParams.url" />
+        image_url:
+        <input type="text" v-model="newProductParams.image_url" />
       </div>
       <div>
-        Width:
-        <input type="text" v-model="newProductParams.width" />
+        description:
+        <input type="text" v-model="newProductParams.description" />
       </div>
       <div>
-        Height:
-        <input type="text" v-model="newProductParams.height" />
+        price:
+        <input type="integer" v-model="newProductParams.price" />
+      </div>
+      <div>
+        category:
+        <input type="text" v-model="newProductParams.category" />
+      </div>
+      <div>
+        tax:
+        <input type="float" v-model="newProductParams.tax" />
+      </div>
+      <div>
+        total:
+        <input type="integer" v-model="newProductParams.total" />
       </div>
       <button type="submit">Create product</button>
     </form>
